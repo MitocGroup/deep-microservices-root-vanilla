26b2496bf87287648e014b5d09df6db71f56a4ca
dsasov <dsasov@mitocgroup.com>
2016-08-01 16:42:32 +0300

    Add tests generated by skeleton

---
+++ src/deep-root-vanilla/tests/backend/test/async-config/dump/AsyncConfig.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Mon Aug 01 2016 16:18:00 GMT+0300 (EEST)
+
+'use strict';
+
+import chai from 'chai';
+import {AsyncConfig} from '../../../../../backend/src/async-config/dump/AsyncConfig';
+
+// @todo: Add more advanced tests
+suite('AsyncConfig', () => {
+  test('Class AsyncConfig exists', () => {
+    chai.expect(AsyncConfig).to.be.an('function');
+  });
+});
